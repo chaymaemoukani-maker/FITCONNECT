ce32f98d3703ac85ec837939183f0840d259cd50
chaymaemoukani-maker <chiamaa.moukani12@gmail.com>
2026-06-25 16:26:27 +0100

    add AdherentService with CRUD operations

---
+++ app/Services/AdherentService.php
@@ -0,0 +1,64 @@
+<?php
+
+class AdherentService
+{
+    private $repository;
+
+    public function __construct($repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function getAll()
+    {
+        return $this->repository->getAll();
+    }
+
+    public function getById($id)
+    {
+        return $this->repository->getById($id);
+    }
+
+    public function add(
+        $nom,
+        $prenom,
+        $email,
+        $password,
+        $telephone,
+        $date,
+        $id_salle
+    ){
+        return $this->repository->add(
+            $nom,
+            $prenom,
+            $email,
+            $password,
+            $telephone,
+            $date,
+            $id_salle
+        );
+    }
+
+    public function update(
+        $id,
+        $nom,
+        $prenom,
+        $email,
+        $telephone,
+        $id_salle
+    ){
+        return $this->repository->update(
+            $id,
+            $nom,
+            $prenom,
+            $email,
+            $telephone,
+            $id_salle
+        );
+    }
+
+    public function delete($id)
+    {
+        return $this->repository->delete($id);
+    }
+}
