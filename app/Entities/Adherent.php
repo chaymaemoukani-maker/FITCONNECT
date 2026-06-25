7c2554e62c42287d27426e78c6feb17c38a7567a
chaymaemoukani-maker <chiamaa.moukani12@gmail.com>
2026-06-25 15:06:31 +0100

    Add Adherent entity model representing database structure

---
+++ app/Entities/Adherent.php
@@ -0,0 +1,13 @@
+<?php
+
+class Adherent
+{
+    public $id_adherent;
+    public $nom;
+    public $prenom;
+    public $email;
+    public $password;
+    public $telephone;
+    public $date_inscription;
+    public $id_salle;
+}
