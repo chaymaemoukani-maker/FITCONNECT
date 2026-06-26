091724c685477c35eeb9bbd7a7eaa424b8bcc3d4
chaymaemoukani-maker <chiamaa.moukani12@gmail.com>
2026-06-26 10:15:34 +0100

    feat: implement AdherentController with CRUD actions

---
+++ app/Controllers/AdherentController.php
@@ -0,0 +1,73 @@
+<?php
+
+require_once __DIR__ . '/../../config/Database.php';
+require_once __DIR__ . '/../Repositories/AdherentRepository.php';
+require_once __DIR__ . '/../Services/AdherentService.php';
+
+class AdherentController
+{
+    private $service;
+
+    public function __construct()
+    {
+        $db = (new Database())->connect();
+
+        $repository = new AdherentRepository($db);
+
+        $this->service = new AdherentService($repository);
+    }
+
+    public function index()
+    {
+        $result = $this->service->getAll();
+
+require __DIR__ . '/../../views/adherents/index.php';    }
+public function create()
+{
+    if(isset($_POST['ajouter']))
+    {
+        $this->service->add(
+            $_POST['nom'],
+            $_POST['prenom'],
+            $_POST['email'],
+            $_POST['password'],
+            $_POST['telephone'],
+            $_POST['date_inscription'],
+            $_POST['id_salle']
+        );
+
+        header("Location: index.php");
+        exit();
+    }
+
+    require __DIR__ . '/../../views/adherents/ajouter.php';
+}
+public function edit($id)
+{
+    if(isset($_POST['modifier']))
+    {
+        $this->service->update(
+            $id,
+            $_POST['nom'],
+            $_POST['prenom'],
+            $_POST['email'],
+            $_POST['telephone'],
+            $_POST['id_salle']
+        );
+
+        header("Location: index.php");
+        exit();
+    }
+
+    $data = $this->service->getById($id);
+
+    require __DIR__ . '/../../views/adherents/modifier.php';
+}
+public function delete($id)
+{
+    $this->service->delete($id);
+
+    header("Location: index.php");
+    exit();
+}
+}
